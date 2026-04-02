d05d222ca1e0f2fcfaaf112e4dea323b328f5e17
Anders Sandström <anders.sandstroem@psi.ch>
2026-04-02 15:11:00 +0200

    Update heat example

--- examples/test/elm3504/elm3504_F32.cmd
+++ examples/test/elm3504/elm3504_F32.cmd
@@ -1,5 +1,5 @@
 
-require ecmccfg sandst_a, "IOC=$(IOC),EC_RATE=1000,ECMC_VER=sandst_a"
+require ecmccfg v11.0.7_RC1, "IOC=$(IOC),EC_RATE=1000,ECMC_VER=v11.0.7_RC1"
 
 # Scalar
 ${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=21, HW_DESC=ELM3504_F32_Scalar"
